caf9df1b4795d4c5fb74bdfdbbfc85bbdbf47de3
EpicSenpai <arvindrathod5476@gmail.com>
2026-07-12 17:07:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ LOGGER = SmartLogger()
 
 # Bot Configuration
 LOG_FILE_NAME = "bot.log"
-PORT = int(os.environ.get("PORT", "7860"))
+PORT = int(os.environ.get("WEB_PORT", "7860"))
 
 OWNER_ID = int(os.environ.get("OWNER_ID", "824793853"))
 MSG_EFFECT = int(os.environ.get("MSG_EFFECT", "5046509860389126442"))
